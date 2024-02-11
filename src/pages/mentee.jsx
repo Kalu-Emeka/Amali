4f487a379921f68a205434505eb54b435d7634b8
Techie <brickkalu@gmail.com>
2024-02-11 08:29:32 +0100

    Css Resolved

--- src/pages/mentee.jsx
+++ src/pages/mentee.jsx
@@ -14,25 +14,25 @@ function Mentee () {
                 <div className="mentor-card">
                     <img src="/images/mentee-1.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>Reem Salem Embroidery Lab</h2>
+                        <h3>Owner: Reem Salem</h3>
+                        <p>Reem Salem Embroidery Lab is a haven for creativity, specializing in handmade Brazilian embroidery. Enthusiasts converge to personalize clothes, accessories, and even furniture, crafting unique pieces that showcase the artistry of this intricate and vibrant embroidery technique.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn1" src="/images/mentee-2.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>Artist</h2>
+                        <h3>Natasha Dahdaleh</h3>
+                        <p>Natasha, a 2008 graduate of Central Saint Martin’s, communicates her passions through art and music. Her works focus on memories and objects that hold personal significance, showcasing beauty in repetition, exemplified by her recurrent use of the butterfly and Islamic art patterns.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn2" src="/images/mentee-3.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>Al-Amal Dairy Products</h2>
+                        <h3>Owner: Amal Shalabi</h3>
+                        <p>Al-Amal Farm is a dairy products factory committed to crafting traditional recipes in a modern approach, using fresh milk. Our range of preservative-free products ensures the finest quality, offering a delightful blend of tradition and contemporary taste.</p>
                     </div>
                 </div>
             </div>
@@ -40,25 +40,25 @@ function Mentee () {
                 <div className="mentor-card">
                     <img src="/images/mentee-4.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>Artist</h2>
+                        <h3>Hala Twal</h3>
+                        <p>Born in 1990 in Amman, Jordan, Hala is a sculptor and painter who earned her B.A. in fine arts/sculpting from the University of Jordan. Inspired by women, nature, and archaeology, she crafts sculptures with various materials and creates paintings and digital collage artworks, exploring the essence of women in their natural context.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn1" src="/images/mentee-5.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>Artist</h2>
+                        <h3>Dina Malkawi</h3>
+                        <p>Born in 1988 in Amman, Jordan, Dina Malkawi earned her Bachelor of Arts degree in Fine Arts from the University of Jordan in 2010. Specializing in oil painting and watercolor, she also explores mixed media on metal and paper. Dina’s art predominantly features human figures, capturing their essence and surroundings through vibrant colors and expressive brushstrokes.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn2" src="/images/mentee-6.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>iOffers Application</h2>
+                        <h3>Owner: Samah Ahmad</h3>
+                        <p>The iOffers application is a secure and free mobile application, offering customers unlimited coupons and exclusive offers on a variety of products and services. Downloadable on smartphones, it provides a user-friendly platform for both customers and vendors.</p> 
                     </div>
                 </div>
             </div>
@@ -66,25 +66,25 @@ function Mentee () {
                 <div className="mentor-card">
                     <img src="/images/mentee-7.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>Finaa Football Academy</h2>
+                        <h3>Owners: Zein Twal and Soleen Zoibi</h3>
+                        <p>Finaa is the pioneering Football Coaching Academy for young girls in the Middle East. It stands out for its emphasis on gender equality and empowering girls through sports, fostering a positive and inclusive environment for their athletic development and personal growth.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn1" src="/images/mentee-8.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>Artist</h2>
+                        <h3>Haneen Shaheen</h3>
+                        <p>Hanin, an Arts educator with 12+ years’ experience, holds a master’s in Curriculum and Instruction and a BA in Visual Arts from the University of Jordan. Fascinated by pedagogies blending visuals and critical thinking, her art journey spans 3 exhibitions, transitioning from academic to abstract, fostering emotional communication through color-layered canvases.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn2" src="/images/mentee-9.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
-                        <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <h2>Artist</h2>
+                        <h3>Tina Omran</h3>
+                        <p>Jordanian painter Tina Omran, with a Bachelor of Arts in Visual Arts from the American University of Cairo, draws inspiration from nightmares, people, objects, landscapes, and abandoned places. Her work explores the interplay between innocence and evil through various characters, weaving intricate backstories and personal interpretations</p>
                     </div>
                 </div>
             </div>
--- src/pages/mentor.jsx
+++ src/pages/mentor.jsx
@@ -22,17 +22,17 @@ function Mentor () {
                 <div className="mentor-card">
                     <img className="menn1" src="/images/mentor-2.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
+                        <h2>Nabila Morcos</h2>
                         <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <p>Nabila Morcos is the founder and CEO of EN-CAPS Consult, a consulting firm specializing in HR services, Developing Performance Management Systems and providing outsourcing services to organizations throughout MENA region. She has been a mentor with Amali Mentoring Network since the start, sharing her insight over HR issues. She studied economics with an GSMP in Organizational Development Strategy from The University of Chicago, USA.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn2" src="/images/mentor-3.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
+                        <h2>Yani Neugebauer</h2>
                         <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <p>Yani Neugebauer is the founder of the consultancy business IHRE MARKTNISCHE in Frankfurt, Germany in 2007. She spent 15 years working in international finance institutions in Project Finance and in Investor Relations. She consults solopreneurs and start-ups over how to build knowledge-based businesses and become known and successful in their market niche. She studied Business and Administration in Munich, German</p>
                     </div>
                 </div>
             </div>
@@ -40,25 +40,25 @@ function Mentor () {
                 <div className="mentor-card">
                     <img src="/images/mentor-4.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
+                        <h2>Dr. Sabine von Oesterreich</h2>
                         <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <p>Sabine von Oesterreich is the founder of a company under her name, where she provides support to investors over the growth of their startups. She has been working as a consultant and coach for managers and teams in well-known organizations, including numerous DAX companies since 2007. She mentors entrepreneurs on how to eliminate losses and disruptive factors. She received her MD from Goethe University in Frankfurt, Germany.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn1" src="/images/mentor-5.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
+                        <h2>HE Marwan Juma</h2>
                         <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <p>Marwan Juma is the founder of “dot.jo,” a web design and online marketing house and “Kinz” for Information Technology, Jordan’s corporate data provider. He is the Vice Chairman at Oasis 500 and Jordan’s former Minister of Information and Communications Technology. He mentors entrepreneurs on startup elements and management. He holds an MS in Business Administration and Management from North Carolina State University.</p>
                     </div>
                 </div>
                 <div className="mentor-card">
                     <img className="menn2" src="/images/mentor-6.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
+                        <h2>Yani Neugebauer</h2>
                         <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <p>Yani Neugebauer is the founder of the consultancy business IHRE MARKTNISCHE in Frankfurt, Germany in 2007. She spent 15 years working in international finance institutions in Project Finance and in Investor Relations. She consults solopreneurs and start-ups over how to build knowledge-based businesses and become known and successful in their market niche. She studied Business and Administration in Munich, German</p>
                     </div>
                 </div>
             </div>
@@ -66,9 +66,9 @@ function Mentor () {
                 <div className="mentor-card">
                     <img className="menn1" src="/images/mentor-7.png" alt="" />
                     <div className="mentor-card-text">
-                        <h2>Rasha Oudeh</h2>
+                        <h2>Marlène Corbun</h2>
                         <h3>Mentor</h3>
-                        <p>Rasha Oudeh is a serial pharmaceutical entrepreneur where her career in the pharmaceutical and healthcare industry began in 2007. She acquired CEDEM AG in Zurich, Switzerland in 2016 and became its CEO. She founded Amali Mentoring Network in 2019 where she mentors entrepreneurs in international business skills. Ms. Oudeh is a Frankfurt School of Finance & Management EMBA graduate.</p>
+                        <p>Marlene Corbun owns her art consultancy business where she is advisor in the fields of modern, post-war and contemporary fine art in Geneva, Switzerland.She mentors entrepreneurs on how to build and develop businesses, especially art collections. She holds a Master in Management from EM Lyon Business School in France and a Post-Graduate Diploma in Art History from The Courtauld Institute in London, UK.</p>
                     </div>
                 </div>
             </div>
